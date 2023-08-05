e089a0da03462b3f568d524f9b81458afa542858
JessBarbosa27 <barbosajess27@gmail.com>
2023-08-05 22:20:03 +0530

    initial commit

--- src/main/java/com/jesslabs/intelEngine/IntelEngineApplication.java
+++ src/main/java/com/jesslabs/intelEngine/IntelEngineApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class IntelEngineApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IntelEngineApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(IntelEngineApplication.class, args);
+    }
 
 }
--- src/main/java/com/jesslabs/intelEngine/openaiGpt_v3_5/controller/GPTv3_5Controller.java
+++ src/main/java/com/jesslabs/intelEngine/openaiGpt_v3_5/controller/GPTv3_5Controller.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
 @RestController
 public class GPTv3_5Controller {
     @Qualifier("openaiRestTemplate")
